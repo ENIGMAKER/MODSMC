061c9e64ccb6242a44a529e3c9f302471b363a96
ENIGMAKER <enigmaker.servers@gmail.com>
2024-11-14 19:42:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,264 @@
+const mods = {
+    terreno: [
+        {
+            name: "Biomes O' Plenty",
+            description: "Añade una gran variedad de nuevos biomas, bloques y efectos visuales al mundo de Minecraft.",
+            images: [
+                "https://i.imgur.com/aiaUH2A.png",
+                "https://cdn.modrinth.com/data/HXF82T3G/images/dddc2bc79b9ff00f831092955b28f6e194a4e8e0.png",
+                "https://cdn.modrinth.com/data/HXF82T3G/images/91bec24289ce9c9151aefd28bb58ae472b42d756.png",
+                "https://cdn.modrinth.com/data/HXF82T3G/images/76b8570c7f9c28b4a83f92d02ea4ed68b861795a.png"
+            ],
+            link: "https://modrinth.com/mod/biomes-o-plenty"
+        },
+        {
+            name: "Oh The Biomes You'll Go",
+            description: "Genera terrenos más realistas y variados, incluyendo montañas, valles y cañones.",
+            images: [
+                "https://i.imgur.com/xbn03uM.png",
+                "https://media.forgecdn.net/attachments/288/667/2020-04-26_00.png",
+                "https://media.forgecdn.net/attachments/319/173/2020-10-31_00.png",
+                "https://media.forgecdn.net/attachments/319/165/2020-10-30_23.png"
+            ],
+            link: "https://www.curseforge.com/minecraft/mc-mods/oh-the-biomes-youll-go"
+        },
+        {
+            name: "Terralith",
+            description: "Mejora la generación de terreno vanilla con nuevos biomas y características.",
+            images: [
+                "https://i.imgur.com/HzgFVlL.png",
+                "https://i.imgur.com/JXwqw7u.png",
+                "https://i.imgur.com/cPec4mY.png"
+            ],
+            link: "https://www.curseforge.com/minecraft/mc-mods/terralith"
+        },
+    ],
+    armaduras: [
+        {
+            name: "Ice and Fire",
+            description: "Añade dragones y otras criaturas míticas, junto con armaduras y armas relacionadas.",
+            images: [
+                "/placeholder.svg?height=300&width=500",
+                "/placeholder.svg?height=300&width=500&text=Ice+and+Fire+2",
+                "/placeholder.svg?height=300&width=500&text=Ice+and+Fire+3"
+            ],
+            link: "https://www.curseforge.com/minecraft/mc-mods/ice-and-fire-dragons"
+        },
+    ],
+    mobs: [
+        {
+            name: "Ice and Fire",
+            description: "Añade dragones y otras criaturas míticas, junto con armaduras y armas relacionadas.",
+            images: [
+                "https://i.imgur.com/cUisgjx.png",
+                "https://cdn.modrinth.com/data/LVnvHVBp/images/7dccf2d9ed9fdc668e21dfb1ce83d1b3bfa7f13a.png",
+                "https://cdn.modrinth.com/data/LVnvHVBp/images/d717be8eda402c2b77096918c62953ecac587fd7.png",
+                "https://cdn.modrinth.com/data/LVnvHVBp/images/c2329c6e321efba28b77a6fe862170323eb5b5fb.png",
+                "https://media.forgecdn.net/attachments/201/23/lnvicra.png"
+            ],
+            link: "https://www.curseforge.com/minecraft/mc-mods/ice-and-fire-dragons"
+        },
+    ],
+    magia: [
+        {
+            name: "Ars Nouveau",
+            description: "Un mod de magia que permite a los jugadores crear sus propios hechizos.",
+            images: [
+                "/placeholder.svg?height=300&width=500",
+                "/placeholder.svg?height=300&width=500&text=Ars+Nouveau+2",
+                "/placeholder.svg?height=300&width=500&text=Ars+Nouveau+3"
+            ],
+            link: "https://www.curseforge.com/minecraft/mc-mods/ars-nouveau"
+        },
+    ],
+    utiles: [
+        {
+            name: "Polymorph",
+            description: "Polymorph es un mod que resuelve los conflictos de recetas permitiendo a los jugadores elegir entre todos los posibles resultados compartidos por los mismos ingredientes.",
+            images: [
+                "https://i.imgur.com/xI5oHIH.png",
+                "https://cdn.modrinth.com/data/tagwiZkJ/images/d6122b7dd0dc7283795513be933a544aa49c3c8d.gif",
+            ],
+            link: "https://modrinth.com/mod/polymorph"
+        },
+    ],
+    visuales: [
+        {
+            name: "3D Skin Layers",
+            description: "Sustituye la segunda capa, normalmente plana, de las pieles de los jugadores por una versión modelada en 3D. Cambiará automáticamente al renderizado vainilla 2d cuando los jugadores estén a más de 12 bloques para aumentar la velocidad de fotogramas. También soporta partes transparentes en cosas como gafas.",
+            images: [
+                "https://raw.githubusercontent.com/tr7zw/3d-Skin-Layers/1.19/img/MC_NU_HEADER.png",
+                "https://cdn.modrinth.com/data/zV5r3pPn/ff7010d4ec0275609866c8b0f603b25e25949c08_96.webp",
+            ],
+            link: "https://modrinth.com/mod/3dskinlayers"
+        },
+        {
+            name: "I Like Wood",
+            description: "Añade variaciones a todas las cosas hechas de madera.",
+            images: [
+                "https://i.imgur.com/dqVZZWU.png",
+                "https://i.ibb.co/xCVDvL3/ezgif-com-gif-maker.gif",
+            ],
+            link: "https://modrinth.com/mod/3dskinlayers"
+        },
+    ],
+    dimensiones: [
+        {
+            name: "BetterEnd",
+            description: "Este mod añade nuevos biomas al End y modifica los ya existentes. Cada bioma tiene su propia atmósfera, recursos y mobs. El cielo tendrá vistas personalizadas con nebulosas púrpuras y estrellas. Todos los biomas tienen música, sonidos y efectos personalizados para hacerlos vivos e interesantes de explorar.",
+            images: [
+                "https://i.imgur.com/9qtMZNg.png",
+                "https://media.forgecdn.net/attachments/317/161/2020-10-17_10.png",
+                "https://media.forgecdn.net/attachments/330/861/2020-12-28_17.png",
+                "https://media.forgecdn.net/attachments/328/795/2020-12-20_21.png"
+            ],
+            link: "https://www.curseforge.com/minecraft/mc-mods/betterend"
+        },
+    ],
+    herramientas: [
+        {
+            name: "Tool Belt",
+            description: "Tool Belt añade un cinturon que permite desplegar un circulo en pantalla donde podras guardar tus herramientas y/o objetos.",
+            images: [
+                "https://i.imgur.com/eIAJaYq.png",
+                "https://i.imgur.com/dtnYVbW.png",
+            ],
+            link: "https://www.curseforge.com/minecraft/mc-mods/ars-nouveau"
+        },
+    ],
+    terror: [
+        {
+            name: "The Silence",
+            description: "...",
+            images: [
+                "https://cdn.modrinth.com/data/BRzYzJex/images/a0506007d81c91110f0e335c84ce5b84de6df138.png",
+            ],
+            link: "https://www.curseforge.com/minecraft/mc-mods/ars-nouveau"
+        },
+    ],
+    biomas: [
+        {
+            name: "Nullscape",
+            description: "Nullscape te hará sentir diminuto y solo en la vasta extensión del Fin. Tú contra el vacío, nada más que eso. Nullscape aumenta la altura del Fin a 384 bloques, y aprovecha al máximo las modernas características de 1.18 para generar un terreno increíblemente variado.",
+            images: [
+                "https://i.imgur.com/1d1ZUxy.png",
+                "https://i.imgur.com/kIrVP9w.jpg",
+                "https://i.imgur.com/3huXoqp.jpg"
+            ],
+            link: "https://modrinth.com/datapack/nullscape"
+        },
+    ],
+};
+
+let currentMod;
+let currentImageIndex = 0;
+
+// Función para ocultar la pantalla de carga y mostrar el contenido
+function hideLoadingScreen() {
+    const loadingScreen = document.getElementById('loading-screen');
+    loadingScreen.classList.add('fade-out');
+    setTimeout(() => {
+        loadingScreen.style.display = 'none';
+        document.getElementById('content').style.display = 'block';
+    }, 1000); // Espera 1 segundo para que termine la animación
+}
+
+// Simular un tiempo de carga
+window.addEventListener('load', () => {
+    setTimeout(hideLoadingScreen, 1000); // 1 segundos de pantalla de carga
+});
+
+function showMods(category) {
+    const modList = document.getElementById('mod-list');
+    modList.innerHTML = ''; // Limpiar la lista actual
+
+    const sortedMods = mods[category].sort((a, b) => a.name.localeCompare(b.name));
+
+    sortedMods.forEach((mod, index) => {
+        const modCard = document.createElement('div');
+        modCard.classList.add('mod-card');
+        modCard.textContent = mod.name;
+        modCard.style.animationDelay = `${index * 0.1}s`;
+        modCard.classList.add('fade-in');
+        modCard.addEventListener('click', () => showModDetails(mod));
+        modList.appendChild(modCard);
+    });
+}
+
+function showModDetails(mod) {
+    currentMod = mod;
+    currentImageIndex = 0;
+    const modal = document.getElementById('mod-details');
+    const title = document.getElementById('mod-title');
+    const image = document.getElementById('mod-image');
+    const description = document.getElementById('mod-description');
+    const link = document.getElementById('mod-link');
+
+    title.textContent = mod.name;
+    image.src = mod.images[0];
+    image.alt = `Imagen de ${mod.name}`;
+    description.textContent = mod.description;
+    link.href = mod.link;
+
+    modal.style.display = 'block';
+    setTimeout(() => {
+        modal.classList.add('show');
+    }, 10);
+
+    updateCarouselButtons();
+}
+
+function updateCarouselButtons() {
+    const prevBtn = document.getElementById('prev-btn');
+    const nextBtn = document.getElementById('next-btn');
+
+    prevBtn.style.display = currentImageIndex > 0 ? 'block' : 'none';
+    nextBtn.style.display = currentImageIndex < currentMod.images.length - 1 ? 'block' : 'none';
+}
+
+function changeImage(direction) {
+    currentImageIndex += direction;
+    if (currentImageIndex < 0) currentImageIndex = 0;
+    if (currentImageIndex >= currentMod.images.length) currentImageIndex = currentMod.images.length - 1;
+
+    const image = document.getElementById('mod-image');
+    image.src = currentMod.images[currentImageIndex];
+    image.alt = `Imagen ${currentImageIndex + 1} de ${currentMod.name}`;
+
+    updateCarouselButtons();
+}
+
+document.querySelectorAll('.category-btn').forEach(button => {
+    button.addEventListener('click', (e) => {
+        const category = e.target.dataset.category;
+        showMods(category);
+    });
+});
+
+// Cerrar el modal
+const modal = document.getElementById('mod-details');
+const span = document.getElementsByClassName('close')[0];
+
+span.onclick = function() {
+    closeModal();
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        closeModal();
+    }
+}
+
+function closeModal() {
+    modal.classList.remove('show');
+    setTimeout(() => {
+        modal.style.display = 'none';
+    }, 300);
+}
+
+// Configurar los botones del carrusel
+document.getElementById('prev-btn').addEventListener('click', () => changeImage(-1));
+document.getElementById('next-btn').addEventListener('click', () => changeImage(1));
+
+// Mostrar los mods de terreno por defecto al cargar la página
+showMods('terreno');
